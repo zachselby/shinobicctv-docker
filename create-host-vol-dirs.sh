27df4a4a26274e687bb823d27ce74483b7d85e72
Zach Selby <zachselby@gmail.com>
2019-10-14 15:26:09 -0500

    Updates to create and store data dirs within ./data, and git ignore it.

--- create-host-vol-dirs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p {shinobiConfig,shinobiCustomAutoLoad,shinobiDatabase,shinobiVideos,shinobiStreams}
-docker-compose up -d
---
+++ setup-and-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p data/{shinobiConfig,shinobiCustomAutoLoad,shinobiDatabase,shinobiVideos,shinobiStreams}
+docker-compose up -d
